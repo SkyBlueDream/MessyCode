86614c15ca442463175f7c8d5a75ad80ed813924
Test One GIT <skybluedream666@gmail.com>
2023-10-03 12:05:58 +0100

    second commit - committing all recap files to the repo

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
+let a ;
+let b ;
+let c ;
 
+document.getElementById("myButton"),onclick = function() {
+    a = document.getElementById("textA").value;
+    a = Number(a);
 
-let userName;
-document.getElementById('mybutton').onclick = function(){
-    userName = document.getElementById("username").value;
-    console.log(userName);
-    document.getElementById("mylabel").innerHTML = userName;
+    b = document.getElementById("textB").value;
+    b = Number(b);
+
+    c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2))
+
+    document.getElementById("cLabel").innerHTML = c;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
